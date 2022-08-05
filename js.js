9f96c7aef74cbb68a8991bec0bf7f5a9a1644662
DemidovID <DemidovID@yandex.ru>
2022-08-06 00:08:21 +0300

    new JS files

---
+++ js.js
@@ -0,0 +1,32 @@
+// alert("Зайди в консоль :)")
+// console.log(":-)");
+
+// var cat;
+
+// cat = 534538475 * 457345348
+
+// console.log(cat)
+
+// var cat = 'My cat likes to sleep', phone = 'iPhone';
+
+// alert(cat);
+// console.log(phone);
+
+var num = 423,
+    string = 'i like tea', 
+    Bool = true, 
+    Null = null, 
+    undef = undefined;
+
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof Bool);
+console.log(typeof Null);
+console.log(typeof undef);
+
+console.log(typeof typeof 1);
+
+// Объектные типы
+
+var obj = {name: "Cat likes to sleep"}, 
+    array = [1, 2, 3]
