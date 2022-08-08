97c4b051aef078eccbd9d4e779edf848041332e7
DemidovID <DemidovID@yandex.ru>
2022-08-08 21:50:00 +0300

    to work later

--- js.js
+++ js.js
@@ -1,4 +1,14 @@
+// to work
+
 (function () {
-    const burgerIten = document.querySelector('.burger__item')
-    const menuItem = document.querySelector('')
+    const burgerIten = document.querySelector('.burger__item');
+    const menuItem = document.querySelector('');
+    const closeMenuItem = document.querySelector('.Burger__close');
+
+    burgerIten.addEventListener('click', () => {
+        menuItem.classList.add('active')
+    });
+    closeMenuItem.addEventListener('click', () => {
+        menuItem.classList.remove('close')
+    });
 }());
